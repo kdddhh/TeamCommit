4433547ea126b579d2d93bd2cc62b0f6cd4468fa
cutePark <cutepark10@naver.com>
2023-05-01 16:03:24 +0900

    hing

--- TeamCommit/src/main/webapp/resource/login/js/login.js
+++ TeamCommit/src/main/webapp/resource/login/js/login.js
@@ -9,3 +9,24 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
+
+// 동의 모두선택 / 해제
+const agreeChkAll = document.querySelector('input[name=agree_all]');
+    agreeChkAll.addEventListener('change', (e) => {
+    let agreeChk = document.querySelectorAll('input[name=agree]');
+    for(let i = 0; i < agreeChk.length; i++){
+      agreeChk[i].checked = e.target.checked;
+    }
+});
+
+function moveClose() {
+  opener.location.href="main.jsp";
+  self.close();
+}
+
+/*$(document).ready(function(){
+	$(".open").on('click'.function(){
+		$(".popup").show();
+	});
+});*/
+
