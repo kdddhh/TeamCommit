e9a8409258788195fe67dab285bdfdd2923d2141
DongHyun <ehdgus6709@naver.com>
2023-05-04 15:33:40 +0900

    lucky letter

--- TeamCommit/src/main/webapp/resource/helper/js/helperInsert.js
+++ TeamCommit/src/main/webapp/resource/helper/js/helperInsert.js
@@ -3,7 +3,7 @@
     function onClick() {
         document.querySelector('.modal_wrap').style.display ='block';
         document.querySelector('.black_bg').style.display ='block';
-        document.querySelector('.selectBox > .select').style.display ='none';
+        document.querySelector('.selectBox > .select').style.overflow ='none';
         document.querySelector('.selectBox > .select').style.display ='none';
         document.querySelector('#matchBox > .select').style.display ='none';
         document.querySelector('#positionBox > .select').style.display ='none';
@@ -11,7 +11,10 @@
     function offClick() {
         document.querySelector('.modal_wrap').style.display ='none';
         document.querySelector('.black_bg').style.display ='none';
-
+        document.querySelector('.selectBox > .select').style.display ='flex';
+        document.querySelector('.selectBox > .select').style.display ='flex';
+        document.querySelector('#matchBox > .select').style.display ='flex';
+        document.querySelector('#positionBox > .select').style.display ='flex';
     }
  
     document.getElementById('reservation').addEventListener('click', onClick);
